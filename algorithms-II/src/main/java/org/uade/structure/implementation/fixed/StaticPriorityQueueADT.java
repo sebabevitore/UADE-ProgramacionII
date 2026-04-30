69ea2ba7401f484f62ffcb924c2d892d5df40968
sebabevitore <sbevitore@uade.edu.ar>
2026-04-30 16:09:05 -0300

    implementacinoes finales

--- algorithms-II/src/main/java/org/uade/structure/implementation/fixed/StaticPriorityQueueADT.java
+++ algorithms-II/src/main/java/org/uade/structure/implementation/fixed/StaticPriorityQueueADT.java
@@ -1,32 +1,78 @@
 package org.uade.structure.implementation.fixed;
 
 
+import org.uade.Exception.EmptyException;
+import org.uade.Exception.OverflowException;
 import org.uade.structure.definition.PriorityQueueADT;
 
+
 public class StaticPriorityQueueADT implements PriorityQueueADT {
+    private int [] values;
+    private int [] priorities;
+    private int count;
+    private final int MAX_SIZE = 1000;
+    public StaticPriorityQueueADT() {
+        this.values = new int[MAX_SIZE];
+        this.priorities = new int[MAX_SIZE];
+        this.count = 0;
+    }
 
     @Override
     public int getElement() {
-        return 0;
+        if(isEmpty()){
+            throw new EmptyException("Priority Queue is empty");
+        }
+        return values[0];
     }
 
     @Override
     public int getPriority() {
-        return 0;
+        if(isEmpty()){
+            throw new EmptyException("Priority Queue is empty");
+        }
+        return priorities[0];
     }
 
     @Override
     public void add(int value, int priority) {
+        if(count == MAX_SIZE){
+            throw new OverflowException("Priority queue is full");
+        }
+
+        int index = 0;
+        // buscar el indice donde debe ir (mientras la prioridad sea menor o igual a la actual)
+        //  >= (los de igual prioridad se encolan atrás)
+        while (index < count && priorities[index] >= priority) {
+            index++;
+        }
+
+        //desplazar desde final hasta index
+        for (int j = count; j > index; j--) {
+            values[j] = values[j - 1];
+            priorities[j] = priorities[j - 1];
+        }
+
+        // ocupar el hueco
+        values[index] = value;
+        priorities[index] = priority;
+        count++;
 
     }
 
     @Override
     public void remove() {
-
+        if (isEmpty()) {
+            throw new EmptyException("Priority Queue is empty");
+        }
+        for (int i = 0; i < count - 1; i++) {
+            values[i] = values[i + 1];
+            priorities[i] = priorities[i + 1];
+        }
+        count--;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return count == 0;
     }
 }
--- algorithms-II/src/main/java/org/uade/structure/implementation/fixed/StaticQueueADT.java
+++ algorithms-II/src/main/java/org/uade/structure/implementation/fixed/StaticQueueADT.java
@@ -29,7 +29,6 @@ public class StaticQueueADT implements QueueADT {
         }
         array[count] = value;
         count++;
-
     }
 
     @Override
