baa5d76f6c300ebb34e5c3abf03df5dd075746d5
sebabevitore <sbevitore@uade.edu.ar>
2026-05-26 15:55:59 -0300

    staticSet ok

---
+++ algorithms-II/src/main/java/org/uade/structure/implementation/fixed/StaticSetADT.java
@@ -0,0 +1,69 @@
+package org.uade.structure.implementation.fixed;
+
+import org.uade.Exception.EmptyADTException;
+import org.uade.Exception.FullADTException;
+import org.uade.structure.definition.SetADT;
+
+import java.util.Random;
+
+public class StaticSetADTPENDIENTE implements SetADT {
+    private int MAX_SIZE = 1000;
+    private int [] values;
+    private int count;
+
+
+    public StaticSetADTPENDIENTE() {
+        this.values = new int [MAX_SIZE];
+        this.count = 0;
+    }
+
+
+    @Override
+    public boolean exist(int value) {
+        for (int i = 0; i < count; i++) {
+            if (values[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int choose() {
+        int randomIndex = new Random().nextInt(count);
+        return values[randomIndex];
+    }
+
+    @Override
+    public void add(int value) {
+        if (count == MAX_SIZE) {
+            throw new FullADTException("Set is full");
+        }
+        if (this.exist(value)) {
+            return;
+        }
+        this.values[count] = value;
+        count++;
+    }
+
+    @Override
+    public void remove(int element) {
+        if (this.isEmpty()) {
+            throw new EmptyADTException("Set is empty");
+        }
+
+        for (int i = 0; i < count; i++) {
+            if (values[i] == element) {
+                // Lo pisamos con el último elemento válido actual
+                this.values[i] = values[count - 1];
+                count--;
+                return;
+            }
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return count == 0;
+    }
+}
